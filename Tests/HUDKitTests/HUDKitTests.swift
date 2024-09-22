173f1624645f077f3994d8e202c026ca80535291
Steffan Andrews <stef@orchetect.com>
2024-09-21 18:19:38 -0700

    Migrated unit tests to Swift Testing

--- Tests/HUDKitTests/HUDKitTests.swift
+++ Tests/HUDKitTests/HUDKitTests.swift
@@ -3,49 +3,44 @@
 //  HUDKit • https://github.com/orchetect/HUDKit
 //
 
-import XCTest
-import AppKit // not needed?
+import Testing
 @testable import HUDKit
-import OTCore
+import AppKit
 
-final class HUDKitTests: XCTestCase {
-    override func setUp() { }
-	
-    func testStyle() {
-        // default - automatic
-		
-        _ = HUD.Style()
-		
-        // presets
-		
-        _ = HUD.Style(.automatic)
-        _ = HUD.Style(.macOS_10_15)
-        _ = HUD.Style(.macOS_11_0)
-		
-        // custom
-		
-        _ = HUD.Style(
-            stickyTime: 3
-        )
-		
-        _ = HUD.Style(
-            stickyTime: 3,
-            position: .bottom,
-            size: .medium,
-            shade: .mediumLight,
-            bordered: false,
-            fadeOut: .defaultDuration
-        )
-    }
-	
-    func testHUD() {
-        dump(NSScreen.screens)
-        print(NSScreen.main?.localizedName as Any)
-        
-        HUD.displayAlert("Test")
-		
-        sleep(2)
-		
-        print("Done")
-    }
+@Test func testStyle() {
+    // default - automatic
+    
+    _ = HUD.Style()
+    
+    // presets
+    
+    _ = HUD.Style(.automatic)
+    _ = HUD.Style(.macOS_10_15)
+    _ = HUD.Style(.macOS_11_0)
+    
+    // custom
+    
+    _ = HUD.Style(
+        stickyTime: 3
+    )
+    
+    _ = HUD.Style(
+        stickyTime: 3,
+        position: .bottom,
+        size: .medium,
+        shade: .mediumLight,
+        bordered: false,
+        fadeOut: .defaultDuration
+    )
+}
+
+@Test func testHUD() {
+    dump(NSScreen.screens)
+    print(NSScreen.main?.localizedName as Any)
+    
+    HUD.displayAlert("Test")
+    
+    sleep(2)
+    
+    print("Done")
 }
