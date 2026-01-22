06a8316909e0efac1c7ade5b78bae553907d2b71
Steffan Andrews <stef@orchetect.com>
2026-01-21 20:56:42 -0800

    `HUDStyle`: Added composable setter methods

--- Sources/SwiftHUD/HUDStyle/HUDStyle+Static.swift
+++ Sources/SwiftHUD/HUDStyle/HUDStyle+Static.swift
@@ -39,3 +39,56 @@ extension HUDStyle {
         transitionOut: .fade(duration: 0.8)
     )
 }
+
+// MARK: - Composable/Chainable Methods
+
+extension HUDStyle {
+    /// Returns the style updating the ``position`` property value.
+    public func position(_ value: Position) -> Self {
+        var copy = self
+        copy.position = value
+        return copy
+    }
+    
+    /// Returns the style updating the ``size`` property value.
+    public func size(_ value: Size) -> Self {
+        var copy = self
+        copy.size = value
+        return copy
+    }
+    
+    /// Returns the style updating the ``tint`` property value.
+    public func tint(_ value: Tint) -> Self {
+        var copy = self
+        copy.tint = value
+        return copy
+    }
+    
+    /// Returns the style updating the ``isBordered`` property value.
+    public func isBordered(_ value: Bool) -> Self {
+        var copy = self
+        copy.isBordered = value
+        return copy
+    }
+    
+    /// Returns the style updating the ``transitionIn`` property value.
+    public func transitionIn(_ value: Transition) -> Self {
+        var copy = self
+        copy.transitionIn = value
+        return copy
+    }
+    
+    /// Returns the style updating the ``duration`` property value.
+    public func duration(_ value: TimeInterval) -> Self {
+        var copy = self
+        copy.duration = value
+        return copy
+    }
+    
+    /// Returns the style updating the ``transitionOut`` property value.
+    public func transitionOut(_ value: Transition) -> Self {
+        var copy = self
+        copy.transitionOut = value
+        return copy
+    }
+}
